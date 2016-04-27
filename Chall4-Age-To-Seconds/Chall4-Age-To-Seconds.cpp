455c981bd9d5f8ed7f9cac1ca8f9eae3f98cb528
Jake Ralph <flooblegoo@gmail.com>
2016-04-27 15:33:23 +1000

    v0.2
    Upgraded version showing roughly what I want my end result to be, but
    based off of dates (user input and current system time) rather than a
    flat year amount. Much work to do. Will still be getting rewritten
    appropraitely.

--- Chall4-Age-To-Seconds/Chall4-Age-To-Seconds.cpp
+++ Chall4-Age-To-Seconds/Chall4-Age-To-Seconds.cpp
@@ -8,9 +8,15 @@
 int main()
 {
 	// Declaring variables needed for calculation (more later for further calculations
-	const int SECONDS_IN_YEAR = 31557600;
+	const int SEC_IN_MIN = 60;
+	const int MIN_IN_HR = 60;
+	const int HRS_IN_DAY = 24;
+	const int DAYS_IN_YR = 365.25;
 	int user_age_years;
 	int user_age_seconds;
+	int user_age_minutes;
+	int user_age_hours;
+	int user_age_days;
 
 	// Outputting program (with super fancy FX (c) to J. Ralph)
 	std::cout << "--------------------------------------" << std::endl;
@@ -32,18 +38,22 @@ int main()
 	// Further case-specific user input validation because of age
 	while (user_age_years >= 117)
 	{
-		std::cout << "Well, considering the oldest person in the world is only 116 years old, pretty hard to believe you are " << user_age_years << " years old!" << std::endl;
-		std::cout << "Try again! Please enter age (years): ";
+		std::cout << "Well, considering the oldest person in the world is only 116 years old, I doubt you are " << user_age_years << " years old!" << std::endl;
+		std::cout << "\nTry again! Please enter age (years): ";
 		std::cin >> user_age_years;
 	}
 
-	// Make calculation of age in seconds
-	user_age_seconds = user_age_years * SECONDS_IN_YEAR;
+	// Make all valid calculations
+	user_age_seconds = SEC_IN_MIN * MIN_IN_HR * HRS_IN_DAY * DAYS_IN_YR * user_age_years;
+	user_age_minutes = MIN_IN_HR * HRS_IN_DAY * DAYS_IN_YR * user_age_years;
+	user_age_hours = HRS_IN_DAY * DAYS_IN_YR * user_age_years;
+	user_age_days = DAYS_IN_YR * user_age_years;
+
 	// And then output the result so it looks all sexy as 
-	std::cout << "\n\nYou are " << user_age_seconds << " seconds old" << std::endl;
+	std::cout << "\nYou are " << user_age_days << " days, " << user_age_hours << " hours, " << user_age_minutes << " minutes and " << user_age_seconds << " seconds old!" << std::endl;
 
 	// Clear buffer, wait for input to close program
-	std::cout << "\n\nPress Enter to Exit . . .";
+	std::cout << "\nPress Enter to Exit . . .";
 	std::cin.clear(); std::cin.ignore(INT_MAX, '\n');
 	std::cin.get();
 
